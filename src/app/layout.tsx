f1348c2a29401729bde1ce2a1c725040a4bd3210
rudra273 <rudramohanty45@gmail.com>
2025-03-13 22:23:29 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,38 @@ export const metadata: Metadata = {
     follow: true,
   },
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      {
+        url: '/favicon_io/favicon.ico',
+      },
+      {
+        url: '/favicon_io/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon_io/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
+    apple: {
+      url: '/favicon_io/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+    },
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/favicon_io/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/favicon_io/android-chrome-512x512.png',
+      },
+    ],
   },
+  manifest: '/favicon_io/site.webmanifest',
 }
 
 
